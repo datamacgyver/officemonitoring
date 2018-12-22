13d5fa05cf379f683031d27ea976aae6798ba944
Rob Mansfield <mansfieldbitter@gmail.com>
2018-12-22 19:31:16 +0000

    Tidying up DB tools while testing

--- DatabaseTools.py
+++ DatabaseTools.py
@@ -1,26 +1,27 @@
 import pyodbc
-import cpu_monitor
 import db_deets as db
+from datetime import datetime
 
 # TODO: Proper excepts
 # TODO: try/catches for failed pushes. What behaviour? . 
 
 
 def get_time():
-    return str(datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d%H%M%S')) 
+    return str(datetime.strftime(datetime.now(), '%Y-%m-%dT%H:%M:%S')) 
         
 class DatabaseTools:  
 
     def __init__(self):
-        self.conn_string = 'Driver={SQL Server};'
+        self.conn_string = ('Driver={SQL Server};' 
                            'Server=%s;'
                            'Database=%s;'
                            'uid=%s;' 
-                           'pwd=%s' % (db.url db.db, db.user, db.passwd)
+                           'pwd=%s') % (db.url, db.db, db.user, db.passwrd	)
 
-        self.conn = pyodbc.connect(self.connString) 
+        self.conn = pyodbc.connect(self.conn_string) 
         self.conn.autocommit = True
         self.cursor = self.conn.cursor()
+        print('Connected to database')
 
     def get_cursor(self):
         try:
@@ -31,18 +32,20 @@ class DatabaseTools:
             return self.conn.cursor()
 
     def db_operation(self, DBstring):
+        print(DBstring)
         try:
             self.cursor.execute(DBstring)
         except:
-            self.getCursor()                
+            self.get_cursor()                
             self.cursor.execute(DBstring)
 
-    def push_value(vals, table, cols):
+    def push_value(self, vals, table, cols):
       cols = ', '.join(cols)
-      vals = ', '.join(vals)
+      vals = ', '.join([str(val) for val in vals])
       
-      cmd = "insert into %s (datetime, %s) values (%s, %s)" % (table, cols, get_time(), vals)
-      db_operation(cmd)
+      cmd = "insert into %s (timestamp, %s) values ('%s', %s)" % (table, cols, get_time(), vals)
+      self.db_operation(cmd)	
       
     def __del__(self):
         self.conn.close()
+        print('Connection Closed')
