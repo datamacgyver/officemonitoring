0757b46eada30eba951cbc7d51df0df12e3669fb
Rob Mansfield <mansfieldbitter@gmail.com>
2018-12-23 09:12:36 +0000

    Breaking monitoring code out into a function for easy replication.

--- monitors.py
+++ monitors.py
@@ -1,16 +1,19 @@
 import subprocess
 
-def get_stub_value():
-  return(12.1)
 
-def get_cpu_temp():
-  bashCommand = "/opt/vc/bin/vcgencmd measure_temp"
-  process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-  output, error = process.communicate()
+def stub(val):
+    return val
 
-  resp = str(output).replace('b"temp=','').replace("'C\\n\"",'')
-  resp = float(resp)
-  return(resp)  # Units are Degrees C
 
-if __name__=="__main__":
-  print(get_stub_value())
+def cpu_temp():
+    bash_command = "/opt/vc/bin/vcgencmd measure_temp"
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+    resp = str(output).replace('b"temp=', '').replace("'C\\n\"", '')
+    resp = float(resp)
+    return resp  # Units are Degrees C
+
+
+if __name__ == "__main__":
+    print(stub())
