617b628fe0149ce40c0ed148401bcb05be403c53
Rob Mansfield <mansfieldbitter@gmail.com>
2018-12-27 20:55:00 +0000

    Adding a loop decorator that runs a function multiple times and takes a median (to prevent odd results).

--- monitors.py
+++ monitors.py
@@ -1,10 +1,27 @@
 import subprocess
+from statistics import median
+from random import randint
+from time import sleep
 
 
-def stub(val):
+def loop_average(func):
+    def wrapper_loop_average():
+        results = []
+        for i in range(1, 10):
+            # TODO: Handle nones. also, do we want to handle 2 returns on t/h?
+            results.append(func())
+            sleep(1)
+        return median(results)
+    return wrapper_loop_average
+
+
+@loop_average
+def stub():
+    val = randint(11, 40)
     return val
 
 
+@loop_average
 def cpu_temp():
     bash_command = "/opt/vc/bin/vcgencmd measure_temp"
     process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
@@ -16,4 +33,4 @@ def cpu_temp():
 
 
 if __name__ == "__main__":
-    print(stub())
+    print(str(stub()))
