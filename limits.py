e7e37a44f753efd87f2ffc20d5704e3bf63de89e
Rob Mansfield <mansfieldbitter@gmail.com>
2018-12-27 23:35:04 +0000

    Moving top/bottom limits to it's own module

---
+++ limits.py
@@ -0,0 +1,5 @@
+stub = {'top': 12.0, 'bottom': 25.0}
+cpu_temp = {'top': 50.0, 'bottom': 20.0}
+
+room_temp = {'top': 5.0, 'bottom': 3.0}
+room_humidity = {'top': 65.0, 'bottom': 0.0}
--- send_updates.py
+++ send_updates.py
@@ -1,19 +1,11 @@
 import requests
 import monitors
 import db_deets
+import limits
 import urllib.parse as parse
 from DatabaseTools import DatabaseTools
 
 ifttt_hook = 'https://maker.ifttt.com/trigger/%s/with/key/%s'
-
-stub_top = 12
-stub_bottom = 25
-cpu_temp_top = 50.0
-cpu_temp_bottom = 20.0
-room_temp_top = 5.0
-room_temp_bottom = 3.0  # TODO: This needs to be altered based on me being there
-room_humidity_top = 70.0
-room_humidity_bottom = 0.0
 
 
 def send_request(name, json=None):
@@ -22,17 +14,13 @@ def send_request(name, json=None):
     requests.post(request, json=json)
 
 
-def run_update(variable, top, bottom, *args):
+def run_update(variable, *args):
     """
 
     Parameters
     ----------
     variable : Str
         function to call to get new value
-    top : float
-        max return value before posting to exceeded webhook
-    bottom : float
-        min return value before posting to lower webhook
     args : args
         additional arguments to pass to func
 
@@ -42,20 +30,21 @@ def run_update(variable, top, bottom, *args):
 
     """
     func = getattr(monitors, variable)
+    limit = getattr(limits, variable)
     val = func(*args)
     db.push_value(val, variable)
-    if val > top:
+    if val > limit['top']:
         send_request(variable + '_above_max')
-    elif val < bottom:
+    elif val < limit['bottom']:
         send_request(variable + '_below_min')
 
 
 try:
     db = DatabaseTools()
-    run_update('stub', stub_top, stub_bottom)
-    run_update('cpu_temp', cpu_temp_top, cpu_temp_bottom)
-    run_update('room_temp', room_temp_top, room_temp_bottom)
-    run_update('room_humidity', room_humidity_top, room_humidity_bottom)
+    run_update('stub')
+    run_update('cpu_temp')
+    run_update('room_temp')
+    run_update('room_humidity')
 except Exception as E:
     send_request('cataclysm_occurred',
                  json={'Value1': parse.quote_plus(str(E))})
