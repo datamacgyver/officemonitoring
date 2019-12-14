bf9dd604e0a72bc544378e69c5f995ddda00320c
Rob Mansfield <mansfieldbitter@gmail.com>
2019-12-14 23:23:58 +0000

    making an S3 connector

---
+++ connectors/s3.py
@@ -0,0 +1,78 @@
+from datetime import datetime
+import logging
+import boto3
+from botocore.exceptions import ClientError
+from secure.logons import s3_bucket, s3_key, s3_secret
+
+
+CLIENT = boto3.client(
+    's3',
+    aws_access_key_id=s3_key,
+    aws_secret_access_key=s3_secret
+)
+
+
+# TODO: try/catches for failed Hive actions. What behaviour?
+def get_time():
+    return str(datetime.strftime(datetime.now(), '%Y%m%dT%H%M%S'))
+
+
+def _upload_file(file_name, bucket, object_name=None):
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    try:
+        _ = CLIENT.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+def _make_temp(a_dict, f_name='temp.json'):
+    a_dict = str(a_dict)
+    with open(f_name, 'w') as f:
+        f.write(a_dict)
+
+    return 'temp.json'
+
+
+def push_new_reading(val, table, col=None):
+
+    col = col if col else table
+    val = str(val)
+    time = get_time()
+
+    out_dict = {'timestamp': time, 'variable': col, 'value': val}
+
+    f = _make_temp(out_dict)
+    _upload_file(f, s3_bucket, object_name='%s/%s.json' % (table, time))
+
+
+def record_hive_command(hive_command):
+    time = get_time()
+    out_dict = {'timestamp': time, ' hive_command':  hive_command}
+
+    f = _make_temp(out_dict)
+    _upload_file(f, s3_bucket, object_name='hivecommands/%s.json' % time)
+
+# def store_latest_value(hive_command, val):
+#     cmd = "select count(*) from lastrecorded where variable = '%s'" % \
+#           hive_command
+#
+#     self._db_search(cmd)
+#     row_count = int(self.cursor.fetchone()[0])
+#
+#     if row_count > 0:
+#         cmd = "UPDATE lastrecorded SET timestamp = '%s', reading = %s " \
+#               " WHERE variable = '%s';" % (get_time(), val, hive_command)
+#         self._db_change(cmd)
+#     else:
+#         cmd = "insert into lastrecorded (timestamp, variable, reading) " \
+#               "values ('%s', '%s', %s)" % (get_time(), hive_command, val)
+#         self._db_change(cmd)
+
+
+if __name__ == "__main__":
+    push_new_reading(0, 'stub')
