471e98fd2cb123c153c999346e1156625025fbaa
Rob Mansfield <mansfieldbitter@gmail.com>
2020-01-12 21:20:33 +0000

    Updating frequency checks to use seconds

--- sensors/frequency_checks.py
+++ sensors/frequency_checks.py
@@ -6,12 +6,17 @@ def _get_time_diff(var):
         time = f.read()
     time = datetime.strptime(time, '%Y-%m-%d %H:%M:%S.%f')
     now = datetime.now()
-    return (now - time).min
+    diff = now-time
+    return diff.seconds / 60
 
 
 def needs_to_run(var, min_diff):
-    diff = _get_time_diff(var)
-    if diff > min_diff:
+    try:
+        diff = _get_time_diff(var)
+    except FileNotFoundError:
+        return True
+
+    if int(diff) > min_diff:
         return True
     else:
         return False
@@ -21,3 +26,10 @@ def note_this_run(var):
     now = datetime.now()
     with open(var+'.timer', 'w') as f:
         return f.write(str(now))
+
+
+if __name__ == '__main__':
+    print(needs_to_run('test', 1))
+    # print(note_this_run('test'))
+    # print(needs_to_run('test', 1))
+    # print(_get_time_diff('test'))
