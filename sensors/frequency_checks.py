2f329f962e6db610b57e77d0e6c8098182e1fea6
Rob Mansfield <mansfieldbitter@gmail.com>
2020-01-12 18:12:23 +0000

    Adding a script to limit sensor check frequency

---
+++ sensors/frequency_checks.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+
+def _get_time_diff(var):
+    with open(var+'.timer', 'r') as f:
+        time = f.read()
+    time = datetime.strptime(time, '%Y-%m-%d %H:%M:%S.%f')
+    now = datetime.now()
+    return (now - time).min
+
+
+def check_if_run(var, min_diff):
+    diff = _get_time_diff(var)
+    if diff > min_diff:
+        return True
+
+
+def mark_ran(var):
+    now = datetime.now()
+    with open(var+'.timer', 'w') as f:
+        return f.write(str(now))
